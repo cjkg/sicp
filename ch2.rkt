8f6231fc6466e27275fd09844ca89c299d0adce2
Christopher Gibilisco <cjgibilisco@gmail.com>
2026-01-15 17:43:37 -0600

    Thru Ex. 2.33

--- ch2.rkt
+++ ch2.rkt
@@ -670,3 +670,14 @@
    *
    1
    (map square (filter odd? sequence))))
+
+;; Exercise 2.33
+
+(define (map2 p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+
+(define (append2 seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length2 sequence)
+  (accumulate (lambda (x y) (inc y)) 0 sequence))
