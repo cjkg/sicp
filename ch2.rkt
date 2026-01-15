5981ee1a9951cbec259cffc94275a7e36897c9ee
Christopher Gibilisco <cjgibilisco@gmail.com>
2026-01-14 23:54:31 -0600

    Thru Ex. 2.32

--- ch2.rkt
+++ ch2.rkt
@@ -529,6 +529,7 @@
 ; and the structure are now lists, so we have to drop the cars from our
 ; selectors:
 
+
 ; (define (right-branch mobile)
 ;   (cdr mobile))
 
@@ -537,3 +538,135 @@
 
 ;;;;;;;;;;;;;;;;
 
+(define (scale-tree-orig tree factor)
+  (cond ((null? tree) nil)
+        ((not (pair? tree))
+         (* tree factor))
+        (else
+         (cons (scale-tree-orig (car tree)
+                                factor)
+               (scale-tree-orig (cdr tree)
+                                factor)))))
+
+
+
+(define (scale-tree tree factor)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree) ; no use of cdr, so no need for null? now
+             (scale-tree sub-tree factor)
+             (* sub-tree factor)))
+       tree))
+
+ (define atree (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+
+;; Exercise 2.30
+
+;define square-tree directly:
+(define (square-tree-direct tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (square tree))
+        (else
+         (cons (square-tree (car tree))
+               (square-tree (cdr tree))))))
+
+;define square-tree in terms of map and recursion:
+(define (square-tree-map tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree-map sub-tree)
+             (square sub-tree)))
+       tree))
+
+;;;;;;;;;;;;;;;;
+
+;; Exercise 2.31
+
+(define (square-tree tree)
+  (tree-map square tree))
+
+(define (tree-map proc tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (tree-map proc sub-tree)
+             (proc sub-tree)))
+       tree))
+
+;;;;;;;;;;;;;;;;
+
+;; Exercise 2.32
+
+; skipped
+
+;;;;;;;;;;;;;;;;
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate
+                       (cdr sequence))))
+        (else (filter predicate
+                      (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op
+                      initial
+                      (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low
+            (enumerate-interval
+             (+ low 1)
+             high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append
+               (enumerate-tree (car tree))
+               (enumerate-tree (cdr tree))))))
+
+(define (sum-odd-squares-signal tree)
+  (accumulate
+   +
+   0
+   (map square
+        (filter odd?
+                (enumerate-tree tree)))))
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
+(define (even-fibs n)
+  (accumulate 
+   cons
+   nil
+   (filter even?
+           (map fib
+                (enumerate-interval 0 n)))))
+
+(define (list-fib-squares n)
+  (accumulate
+   cons
+   nil
+   (map square
+        (map fib
+             (enumerate-interval 0 n)))))
+
+(define
+  (product-of-squares-of-odd-elements
+   sequence)
+  (accumulate
+   *
+   1
+   (map square (filter odd? sequence))))
